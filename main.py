609a740ae1bc2633a2bc2686c3737b688a78b15c
Tomasz Graczyk <tgraczyk86@gmail.com>
2024-03-28 17:36:28 +0100

    Palindrom

---
+++ main.py
@@ -0,0 +1,9 @@
+def palindrom(word):
+    word = word.lower()
+    backword="".join(reversed(word))
+
+    if word.isalnum():
+        print("Czy to słowo jest palindromem?")
+        print(word==backword)
+
+palindrom("kukułka")
